c589335db31da6deb6decb8073782586d143d123
Eliad Karni <EliadK995@Gmail.com>
2022-04-05 20:49:45 +0300

    Documentation

--- harry rational.py
+++ harry rational.py
@@ -2,7 +2,8 @@ import re
 from os.path import exists
 from os import rename
 """
-The code find
+The code finds in each file its chapter name and chapter number.
+Then its changes the file's name into it it the syntax of three zfill of the chapter number, and the chapter name.
 
 
 files exist check: https://www.pythontutorial.net/python-basics/python-check-if-file-exists/ 
@@ -13,14 +14,24 @@ clean special characters from string: https://stackoverflow.com/questions/584351
 NUM_OF_FILES = 122
 
 
-def rename_files():
+def rename_files() -> None:
+    """
+    The function renames each html file into the syntax: chapter name in three zfill syntax and the chapter name.
+    :return: None
+    """
     for i in range(NUM_OF_FILES):
         if exists(f"{i}.html"):
             name = get_name(f"{i}.html")
             rename(f"{i}.html", f"{name}.html")
 
 
-def get_name(file_name: str):
+def get_name(file_name: str) -> str:
+    """
+    The function finds the received file's chapter number and name.
+    Then it returns string of the chapter in syntax of: three zfill chapter number and then the chapter name.
+    :param file_name: The tested file name.
+    :return: The chapter in syntax of: three zfill chapter number and then the chapter name.
+    """
     with open(file_name, 'r') as reader:
         for line in reader:
             if re.search("<title>.*</title>", line):
